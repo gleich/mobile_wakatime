cbed4c568a0df6e4eadf199a1ace6eb5b16c3785
Matthew Gleich <matthewgleich@gmail.com>
2020-05-28 23:38:35 -0400

    💡 Sort imports
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
+// 🐦 Flutter imports:
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Wakatime',
       theme: ThemeData(
         // This is the theme of your application.
         //
